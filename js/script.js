a1d9ef714aa09eb0f9f39ab4716189522a29d4ab
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 15:27:53 +0100

    creato ciclo for con all'interno condizione if

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@
 // per i multipli di 3 deve uscire Fizz
 // per i multipli di 5 deve uscire Buzz
 // per i multipli di 3 e 5 deve uscire FizzBuzz
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 10; i++) {
+
     console.log(i)
-    
+
+
+    if (i / 3) {
+        i = ('Fizz')
+    } else if (i / 5) {
+        i = ('Buzz')
+    }
 }
+
+
+

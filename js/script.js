8217512a6ef1cd24b6910a2430483790e09c252a
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 16:24:12 +0100

    creata correttamente if all'interno del for grazie al modulo

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@
 // per i multipli di 3 deve uscire Fizz
 // per i multipli di 5 deve uscire Buzz
 // per i multipli di 3 e 5 deve uscire FizzBuzz
-for (let i = 1; i <= 10; i++) {
-
-    console.log(i)
 
 
-    if (i / 3) {
-        i = ('Fizz')
-    } else if (i / 5) {
-        i = ('Buzz')
+for (let index = 1; index <= 100; index++) {
+  
+   
+    if (index % 3 === 0 && index % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (index % 5 === 0) {
+        console.log('Buzz')
+    } else if (index % 3 === 0) {
+        console.log('Fizz')
+    } else {
+        console.log(index)
     }
+
+    
 }
 
 

cedb261bcd1778ecab0df59ef420a5a427fb9539
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-14 15:00:41 +0100

    mostrato in console numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// scrivere in console numeri da 1 a 100
+// per i multipli di 3 deve uscire Fizz
+// per i multipli di 5 deve uscire Buzz
+// per i multipli di 3 e 5 deve uscire FizzBuzz
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+    
+}
